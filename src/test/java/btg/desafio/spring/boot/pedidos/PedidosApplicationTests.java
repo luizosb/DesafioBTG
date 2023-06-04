088df4244e40e1e15638ad81a166055b467f200f
luizosb <luiz-osb@hotmail.com>
2023-06-03 23:23:15 -0300

    Começo do projeto

---
+++ src/test/java/btg/desafio/spring/boot/pedidos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package btg.desafio.spring.boot.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
